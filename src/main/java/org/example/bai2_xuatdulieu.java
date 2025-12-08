42f29439d498973d61072230b35c429a053cc666
ThinhTran <15520847@gm.uit.edu.vn>
2025-12-08 22:54:42 +0700

    c1 - Test Git

---
+++ src/main/java/org/example/bai2_xuatdulieu.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class bai2_xuatdulieu {
+    static void main(String[] args) {
+        System.out.println("Dong 1");
+        System.out.println("Dong 2");
+        System.out.println("Dong 3");
+
+        //print
+        System.out.print("dong 4");
+        System.out.print("dong 5");
+
+    }
+}
