e88c2f32e4752086ae45dd1764c3b159c00aa177
ThinhTran <15520847@gm.uit.edu.vn>
2025-12-24 21:42:17 +0700

    C3 - bai4

---
+++ src/main/java/org/example/bai4_Kieudulieu.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class bai4_Kieudulieu {
+    public static void main(String[] args) {
+        //kiểm tra kiểu nguyên
+        System.out.println("max của kiểu int " +Integer.MAX_VALUE);
+        System.out.println("min của kiểu int " +Integer.MIN_VALUE);
+
+        //kiểm tra kiểu Byte
+        System.out.println("max của kiểu byte " +Byte.MAX_VALUE);
+        System.out.println("min của kiểu byte " +Byte.MIN_VALUE);
+    }
+}
