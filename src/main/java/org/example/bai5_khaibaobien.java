093d858bc40b6df34a114a30b8431a28afddcde2
ThinhTran <15520847@gm.uit.edu.vn>
2025-12-24 22:20:40 +0700

    C4 - epKieudulieu

---
+++ src/main/java/org/example/bai5_khaibaobien.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class bai5_khaibaobien {
+    static void main(String[] args) {
+        //khai bao bien
+        int a;
+        byte tuoi;
+        float diemToan;
+
+        //khai bao nhieu bien 1 luc cung kieu
+        int b, c, d, e;
+
+        //khoi tao bien
+        int tuoiCon = 10;
+        float diemVan = 7.5f;
+        double diemAnh = 9.5;
+        System.out.println(tuoiCon);
+        tuoiCon = 15;
+        System.out.println(tuoiCon);
+
+        System.out.println(diemVan);
+        System.out.println(diemAnh);
+
+        //hang so
+        final int DOSOI=100;
+    }
+}
---
+++ src/main/java/org/example/bai6_epkieudulieu.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class bai6_epkieudulieu {
+    static void main(String[] args) {
+        int a = 6, b = 12;
+        double ketQua = (double) a/b; //int -> double
+        System.out.println("ket qua a/b: " +ketQua);
+
+        //ep kieu hep
+        int c = 128;
+        byte d = (byte) c;
+        System.out.println("c: " +c);
+        System.out.println("d: " +d); // kieu byte tu -128 den 127, nen khi ep kieu byte cho c bi that thoat du lieu -> sai
+
+        int e = 15;
+        byte f = (byte) e; // ep kieu tu int -> byte
+        System.out.println("e: " +e);
+        System.out.println("f: " +f);
+        // Note: co the that thoat du lieu khi ep kieu hep
+    }
+}
