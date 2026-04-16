919cac33660a8326e86140189743d05580aa7050
ThinhTran <15520847@gm.uit.edu.vn>
2026-04-16 17:05:03 +0700

    2026-04-16: feat: separate letters and numbers using Regex

--- src/main/java/org/example/StringSeparator.java
+++ src/main/java/org/example/StringSeparator.java
@@ -1,4 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class StringSeparator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao chuoi can tach: ");
+        String str = sc.nextLine();
+
+        //tach chuoi chu: xoa tat ca nhung gi khong phai la chu cai
+        String letters = str.replaceAll("[^a-zA-Z]", "");
+        //tach chuoio so: xoa tat ca nhung gi khong phai la so
+        String numbers = str.replaceAll("[^0-9]", "");
+
+        System.out.println("Chuoi ky tu da tach la: " + letters);
+        System.out.println("Chuoi so da tach la: " + numbers);
+    }
 }
